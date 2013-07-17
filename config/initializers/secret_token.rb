e4c17298ea333f9ce869fdce2f4c4c9452e6202c
Jason Jin <realjasonjin@gmail.com>
2013-07-16 18:21:48 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RybApp::Application.config.secret_token = '576dcd0080e977da0a79a171ba6c4166a36cbd79fe056c53688610b8e5653ae436a2e4c449447167aef631bd679b7dbc1d333cdbc89ec3e9abb2f6451687d0f1'
